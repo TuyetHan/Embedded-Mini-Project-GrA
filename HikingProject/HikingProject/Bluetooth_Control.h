966c8d0972c00edfaf7c71410d977971e35afcd1
TuyetHan <44611883+TuyetHan@users.noreply.github.com>
2023-03-20 12:06:19 +0200

    Optimization

---
+++ HikingProject/HikingProject/Bluetooth_Control.h
@@ -0,0 +1,9 @@
+#ifndef BLUETOOTH_CONTROL_H
+#define BLUETOOTH_CONTROL_H
+#include <Arduino.h>
+#include "config.h"
+
+void Bluetooth_Initialize();
+void Send_Hikingdata_Bluetooth();
+
+#endif
---
+++ HikingProject/HikingProject/WatchDisplay.h
@@ -0,0 +1,46 @@
+#ifndef WATCHDISPLAY_H
+#define WATCHDISPLAY_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "freertos/queue.h"
+#include <soc/rtc.h>
+#include "esp_sleep.h"
+
+typedef enum {
+    LV_ICON_BAT_EMPTY,
+    LV_ICON_BAT_1,
+    LV_ICON_BAT_2,
+    LV_ICON_BAT_3,
+    LV_ICON_BAT_FULL,
+    LV_ICON_CHARGE,
+    LV_ICON_CALCULATION
+} lv_icon_battery_t;
+
+typedef enum {
+    LV_STATUS_BAR_BATTERY_LEVEL = 0,
+    LV_STATUS_BAR_BATTERY_ICON = 1,
+    LV_STATUS_BAR_WIFI = 2,
+    LV_STATUS_BAR_BLUETOOTH = 3,
+} lv_icon_status_bar_t;
+
+enum {
+    Q_EVENT_BMA_INT,
+    Q_EVENT_AXP_INT,
+} ;
+
+void updateBatteryIcon(lv_icon_battery_t index);
+void updateBatteryLevel();
+void updateStepCount();
+void updateDistance();
+void updateHikingTime();
+void updateCalories();
+
+void setupGui();
+void GUI_Initialize();
+void Main_GUI_Handler();
+
+extern void Reset_Hiking_Data();
+
+#endif /*__GUI_H */
